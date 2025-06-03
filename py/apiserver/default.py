e3e7b11cae71c05ee950c558d07122023adf7d7a
djfjcen <13872004+hduwdwang@user.noreply.gitee.com>
2025-06-03 21:47:47 +0800

    修改豆包调用错误

--- py/apiserver/default.py
+++ py/apiserver/default.py
@@ -1,13 +1,13 @@
 import os
 from openai import OpenAI
-def get_chat_completion(api_key,base_url,model, messages,temperature=0.7):
+def get_chat_completion(api_key,base_url,model_type, promotes,temperature=0.7):
     client = OpenAI(
         api_key="sk-176d442796bf4b4f9cf28afdb03d25ae",
         base_url="https://api.deepseek.com"
     )
     completion = client.chat.completions.create(
-        model=model,
-        messages=messages,
+        model=model_type,
+        messages=promotes,
         stream=True,
         temperature=temperature
     )
--- py/apiserver/http_to_wss_server.py
+++ py/apiserver/http_to_wss_server.py
@@ -216,13 +216,13 @@ async def handle_wss_stream(data: dict, request: web.Request):
                         )
                     case "doubao-1-5-pro":
                         result = doubao.doubao_completion(
-                            model="doubao-1-5-pro",
+                            # model="doubao-1-5-pro",
                             messages=promotes,
                             temperature=temperature,
                         )
                     case "doubao-1-5-thinking-pro":
                         result = doubao.doubao_reasoner(
-                            model="doubao-1-5-thinking-pro",
+                            # model="doubao-1-5-thinking-pro",
                             messages=promotes,
                             temperature=temperature,
                         )
@@ -266,7 +266,7 @@ async def handle_wss_stream(data: dict, request: web.Request):
                         )
                     case _:
                         result = default.get_chat_completion(
-                            api_key=api_key, URL=URL, model_type=model_type, promotes=promotes, temperature=temperature
+                            api_key=api_key, base_url=URL, model_type=model_type, promotes=promotes, temperature=temperature
                         )
 
                 has_sent_reasoning_header = False
@@ -336,11 +336,11 @@ async def handle_send_ans(request: web.Request):
             return web.json_response({"error": 3002})
 
         global prompt_data
-        prompt_data = await fetch_message_history(0, session_id)
-        print(f"获取的 prompt_data: {prompt_data}")  # 打印原始历史记录
-        error = prompt_data.get("error")
-        if not prompt_data or error != 0:
-            return web.json_response({"error": 3007})
+        # prompt_data = await fetch_message_history(0, session_id)
+        # print(f"获取的 prompt_data: {prompt_data}")  # 打印原始历史记录
+        # error = prompt_data.get("error")
+        # if not prompt_data or error != 0:
+        #     return web.json_response({"error": 3007})
 
         asyncio.create_task(handle_wss_stream(data, request))
 
